d6f1cc09de35507708295ea359a234e0b40c9d02
Backengine codegen <codegen@backengine.dev>
2024-03-11 23:47:49 +0000

    Backengine codegen

--- src/public_endpoints_config.py
+++ src/public_endpoints_config.py
@@ -1,1 +1,10 @@
-PUBLIC_ENDPOINTS: list[str] = []
+# This configuration file defines which endpoints do not require authorization.
+
+# Add endpoints that do not require authentication here
+PUBLIC_ENDPOINTS = [
+    "/healthcheck",
+    "/login",
+    "/signup",
+    "/hello/{first_name}",
+    "/loaderio-bffbe3f257bed1bcc16ed5e1257bac75.txt"
+]
